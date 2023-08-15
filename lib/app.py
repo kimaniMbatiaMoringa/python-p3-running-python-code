d7bada016c8b25fc5556402e0987738bea31fe63
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-08-15 12:04:29 +0300

    assignment completed

---
+++ lib/app.py
@@ -0,0 +1,19 @@
+#Use the "#" key to create a comment
+
+#install python on ubuntu using sudo apt install python-is-python3
+
+#Check python version by typing "python --version"
+
+print("Hello World! Pass this test, please.") #Equivalent of "console.log() in javascript"
+#print("Hello sun!", end="TOO HOT DOE ")
+#print("Hello sky", end="!!!")
+
+#execute python by typing "python appName.py"
+
+#You can use the python shell which is a mini repl by typing "python"
+#Then type your code in the >>> area
+#you can leave the python shell by typing exit() or pressing ctrl+d
+
+#Each lesson needs some python libraries which are documented in the Pipfile
+#Pipfile is a sort of Package.json but for python
+#Type pipenv install to install dependencies
